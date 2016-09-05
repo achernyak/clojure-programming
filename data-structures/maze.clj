5abe657c273dbb6b674f7b0f02195b33194c9294
achernyak <artem@ikayzo.com>
2016-09-05 18:46:32 -0500

    willson's maze

--- data-structures/maze.clj
+++ data-structures/maze.clj
@@ -12,6 +12,22 @@
                  (reduce disj unvisited (keys steps))))
         walls))))
 
+(defn wmaze [walls]
+  (let [paths (reduce (fn [index [a b]]
+                        (merge-with into index {a [b] b [a]}))
+                      {} (map seq walls))
+        start-loc (rand-nth (keys paths))]
+    (loop [walls walls
+           unvisited (disj (set (keys paths)) start-loc)]
+      (if-let [loc (when-let [s (seq unvisited)] (rand-nth s))]
+        (let [walk (iterate (comp rand-nth paths) loc)
+              steps (zipmap (take-while unvisited walk) (next walk))
+              walk (take-while identity (iterate steps loc))
+              steps (zipmap walk (next walk))]
+          (recur (reduce disj walls (map set steps))
+                 (reduce disj unvisited (keys steps))))
+        walls))))
+
 (defn grid [w h]
   (set (concat
         (for [i (range (dec w)) j (range h)] #{[i j] [(inc i) j]})
