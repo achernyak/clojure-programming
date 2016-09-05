2ed6d0b551e300c838da398d2c7224a903f78aca
achernyak <artem@ikayzo.com>
2016-09-05 13:53:29 -0500

    timestamped logger

--- hof/logger.clj
+++ hof/logger.clj
@@ -12,3 +12,19 @@
   #(with-open [f (clojure.java.io/writer file :append true)]
      ((print-logger f) %)))
 
+(defn multi-logger
+  [& logger-fns]
+  #(doseq [f logger-fns]
+     (f %)))
+
+(def log (multi-logger
+          (print-logger *out*)
+          (file-logger "messages.log")))
+
+(defn timestamped-logger [logger]
+  #(logger (format "[%1$tY-%1$tm-%1$te %1$tH:%1$tM:%1$tS] %2$s" (java.util.Date.) %)))
+
+(def log-timestamped (timestamped-logger
+                      (multi-logger
+                       (print-logger *out*)
+                       (file-logger "messages.log"))))
