0a06fc586169938cbe4d4c4070d48f78288e82a5
achernyak <artemchernyak@gmail.com>
2016-09-16 11:24:10 -0500

    started macros

---
+++ macros/macros.clj
@@ -0,0 +1,13 @@
+(require '(clojure [string :as str]
+                   [walk :as walk]))
+
+(defmacro reverse-it
+  [form]
+  (walk/postwalk #(if (symbol? %)
+                    (symbol (str/reverse (name %)))
+                    %)
+                 form))
+
+(reverse-it
+ (qesod [gra (egnar 5)]
+        (nltnirp (cni gra))))
