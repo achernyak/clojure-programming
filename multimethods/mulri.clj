d5c71b8cc95b532e535d13f64e9a4f1134cc70ec
achernyak <artemchernyak@gmail.com>
2016-09-23 08:16:48 -0500

    repetative multi

--- multimethods/mulri.clj
+++ multimethods/mulri.clj
@@ -1,26 +1,52 @@
+(ns-unmap *ns* 'fill)
+
+(defn- fill-dispatch [node value]
+  (if (= :input (:tag node))
+    [(:tag node) (-> node :attrs :type)]
+    (:tag node)))
+
 (defmulti fill
   "Fill a xml/html node (as per clojure.xml)
   with the provided value."
-  (fn [node value] (:tag node))
+  #'fill-dispatch
   :default nil)
 
-(defmethod fill :div
+(defmethod fill nil
   [node value]
   (assoc node :content [(str value)]))
 
-(defmethod fill :input
+(defmethod fill [:input nil]
   [node value]
   (assoc-in node [:attrs :value] (str value)))
 
-(fill {:tag :div} "hello")
-(fill {:tag :input} "hello")
+(defmethod fill [:input "hidden"]
+  [node value]
+  (assoc-in node [:attrs :value] (str value)))
+
+(defmethod fill [:input "text"]
+  [node value]
+  (assoc-in node [:attrs :value] (str value)))
+
+(defmethod fill [:input "radio"]
+  [node value]
+  (if (= value (-> node :attrs :value))
+    (assoc-in node [:attrs :checked] "checked")
+    (update-in node [:attrs] dissoc :checked)))
+
+(defmethod fill [:input "checkbox"]
+  [node value]
+  (if (= value (-> node :attrs :value))
+    (assoc-in node [:attrs :checked] "checked")
+    (update-in node [:attrs] dissoc :checked)))
 
 (defmethod fill :default
   [node value]
   (assoc-in node [:attrs :value] [(str value)]))
 
-(fill {:span :input} "hello")
+(fill {:tag :input
+       :attrs {:value "first choice"
+               :type "checkbox"}}
+      "first choice")
 
-(defmethod fill nil
-  [node value]
-  (assoc node :content [(str value)]))
+(fill *1 "off")
+
