ade13dc0e8251301b14fe790f39321fa7327adf8
achernyak <artemchernyak@gmail.com>
2016-09-23 08:46:06 -0500

    simpler solution

--- multimethods/mulri.clj
+++ multimethods/mulri.clj
@@ -1,44 +1,38 @@
 (ns-unmap *ns* 'fill)
 
-(def fill-hierarchy (-> (make-hierarchy)
+(def input-hierarchy (-> (make-hierarchy)
                         (derive :input.radio ::checkable)
-                        (derive :input.checkbox ::checkable)
-                        (derive ::checkable :input)
-                        (derive :input.text :input)
-                        (derive :input.hidden :input)))
+                        (derive :input.checkbox ::checkable)))
 
 (defn- fill-dispatch [node value]
-  (if-let [type (and (= :input (:tag node))
-                     (-> node :attrs :type))]
-    (keyword (str "input." type))
-    (:tag node)))
+    (:tag node))
 
 (defmulti fill
   "Fill a xml/html node (as per clojure.xml)
   with the provided value."
   #'fill-dispatch
-  :default nil
-  :hierarchy #'fill-hierarchy)
+  :default nil)
 
-(defmethod fill nil
+(defmulti fill-input
+  "Fill an input field."
+  (fn [node value] (-> node :attrs :type))
+  :default nil
+  :hierarchy #'input-hierarchy)
+
+(defmethod fill nil [node value]
   [node value]
   (assoc node :content [(str value)]))
 
 (defmethod fill :input [node value]
-  (assoc-in [:attrs :value] (str value)))
+  (fill-input node value))
 
-(defmethod fill ::checkable [node value]
+(defmethod fill-input nil [node value]
+  (assoc-in node [:attrs :value] (str value)))
+
+(defmethod fill-input ::checkable [node value]
   (if (= value (-> node :attrs :value))
     (assoc-in node [:attrs :checked] "checked")
     (update-in node [:attrs] dissoc :checked)))
-
-(defmethod fill nil [node value]
-  (if (= :input (:tag node))
-    (do
-      (alter-var-root #'fill-hierarchy
-                      derive (fill-dispatch node value) :input)
-      (fill node value))
-    (assoc node :content [(str value)])))
 
 (fill {:tag :input
        :attrs {:value "first choice"
@@ -47,3 +41,6 @@
 
 (fill *1 "off")
 
+(fill {:tag :input
+       :attrs {:type "date"}}
+      "20110820")
